12e1feca9d52c2a9bc4bdf12cd5338db9b35c73f
Andrew Subotin <subotinandrey5@gmail.com>
2023-01-13 16:25:31 +0200

    movies: customize admin

--- vidly/movies/admin.py
+++ vidly/movies/admin.py
@@ -6,5 +6,9 @@ class GenreAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
 
 
+class MovieAdmin(admin.ModelAdmin):
+    exclude = ('date_created', )
+
+
 admin.site.register(Genre, GenreAdmin)
-admin.site.register(Movie)
+admin.site.register(Movie, MovieAdmin)
